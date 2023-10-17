6fdb789c708dface954d35901ff7417f0b440e57
AviduDev <avidudev@gmail.com>
2023-10-17 23:16:04 +0530

    Sitemap added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
 
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   image: {
-    domains: ["res.cloudinary.com"],
+    domains: ["res.cloudinary.com"]
   },
+  site: 'https://avidu.me',
+  integrations: [sitemap()]
 });
